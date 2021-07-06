b30398d84dfc45f9d97eddfb53bb0846c4629924
Visal Rahmanov <rahmanovvisal@gmail.com>
2021-07-06 15:14:42 +0300

    исправляет ошибки в скриптах

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const input = document.getElementById('font-size-control');
 const span = document.getElementById('text');
 input.addEventListener("input", handleInputRange);
 function handleInputRange(event) {
-  span.style.fontSize = event.currentTarget.value + 'px';
+  span.style.fontSize = event.currentTarget.value * 0.32 + 'px';
 }
